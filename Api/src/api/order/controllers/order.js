3f69e7914785ea8929986fee4d179a8e63ac50ed
Harshal Margaj <apple@Apples-MacBook-Air.local>
2024-12-09 12:10:56 +0530

    Updated package files and order controller

--- Api/src/api/order/controllers/order.js
+++ Api/src/api/order/controllers/order.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
-/**
- * order controller
- */
+const stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products } = ctx.request.body;
+
+    const lineItems = await Promise.all(
+      products.map(async (item) => {
+        const item = await strapi
+          .service("api::product.product")
+          .findOne(item.id);
+      })
+    );
+
+    try {
+      const session = stripe.checkout.create({
+        mode: "payment",
+        success_url: `${process.env.CLIENT_URL}?success=true`,
+        cancel_url: `${process.env.CLIENT_URL}?success=false`,
+      });
+    } catch (error) {}
+  },
+}));
