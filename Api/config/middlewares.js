3fcc3a51179d8d4a1937fde45d75c07c79b83693
Harshal Margaj <apple@Apples-MacBook-Air.local>
2024-12-11 18:42:32 +0530

    fix cors-1

--- Api/config/middlewares.js
+++ Api/config/middlewares.js
@@ -2,17 +2,7 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "http://localhost:5173",
-        "https://clothesofshopvista.netlify.app/",
-      ], // Allow your frontend URLs
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // HTTP methods to allow
-      headers: ["Content-Type", "Authorization"], // Allow required headers
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
