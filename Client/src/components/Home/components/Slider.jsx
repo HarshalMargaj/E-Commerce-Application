ae60bfabc7e342445374e4b1b49a036fa050dd66
Harshal Margaj <apple@Apples-MacBook-Air.local>
2024-12-12 19:20:03 +0530

    changed banners

--- Client/src/components/Home/components/Slider.jsx
+++ Client/src/components/Home/components/Slider.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import ImageOne from "../../../assets/3588607.jpg";
-import ImageTwo from "../../../assets/3609477.jpg";
-// import ImageThree from "../../../assets/4028471.jpg";
+import ImageOne from "../../../assets/Screenshot 2024-12-12 at 7.16.37 PM.png";
+import ImageTwo from "../../../assets/Screenshot 2024-12-12 at 7.16.46 PM.png";
+import ImageThree from "../../../assets/Screenshot 2024-12-12 at 7.16.55 PM.png";
 // import ImageFour from "../../../assets/4028484.jpg";
 // import ImageFive from "../../../assets/imagefive.jpg";
 // Import Swiper styles
@@ -39,9 +39,10 @@ export default function App() {
 				<SwiperSlide>
 					<img src={ImageTwo} alt="" />
 				</SwiperSlide>
-				{/* <SwiperSlide>
+				<SwiperSlide>
 					<img src={ImageThree} alt="" />
 				</SwiperSlide>
+				{/* 
 				<SwiperSlide>
 					<img src={ImageFour} alt="" />
 				</SwiperSlide> */}
