a380ef5e88c07c9c737e043ec7567f6a3cc67c04
Harshal Margaj <harshal.margaj12@gmail.com>
2025-12-17 12:16:00 +0530

    update database.js to use connection url

--- Api/config/database.js
+++ Api/config/database.js
@@ -1,12 +1,30 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "postgres"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD"),
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//       schema: env("DATABASE_SCHEMA", "public"),
+//     },
+//     pool: {
+//       min: env.int("DATABASE_POOL_MIN", 2),
+//       max: env.int("DATABASE_POOL_MAX", 10),
+//     },
+//     acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD"),
+      connectionString: env("DATABASE_URL"),
       ssl: {
         rejectUnauthorized: false,
       },
