da1cbb9b0c63244925f1cf07a1d6864b635e739c
Harshal Margaj <harshal.margaj12@gmail.com>
2025-12-17 15:36:22 +0530

    use session pooler connection string

--- Api/config/database.js
+++ Api/config/database.js
@@ -1,31 +1,13 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", false),
-      schema: env("DATABASE_SCHEMA", "public"),
-    },
-    pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
-    },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-  },
-});
-
 // module.exports = ({ env }) => ({
 //   connection: {
 //     client: "postgres",
 //     connection: {
-//       connectionString: env("DATABASE_URL"),
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "postgres"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD"),
+//       ssl: env.bool("DATABASE_SSL", false),
 //       schema: env("DATABASE_SCHEMA", "public"),
 //     },
 //     pool: {
@@ -35,3 +17,24 @@ module.exports = ({ env }) => ({
 //     acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
 //   },
 // });
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      connectionString: env(
+        "DATABASE_URL",
+        "postgresql://postgres.fnlbzzmjhumeepdnbdth:1234567890@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
+      ),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
+    },
+    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+  },
+});
