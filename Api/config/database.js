f8ec5201b5f7a9a676f3ff580d67761700992b53
Harshal Margaj <harshal.margaj12@gmail.com>
2025-12-17 16:08:53 +0530

    fix pool limit

--- Api/config/database.js
+++ Api/config/database.js
@@ -32,8 +32,8 @@ module.exports = ({ env }) => ({
       schema: env("DATABASE_SCHEMA", "public"),
     },
     pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
+      min: 0,
+      max: 2, // 🔴 DO NOT go above 3 on Supabase free tier
     },
     acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
   },
